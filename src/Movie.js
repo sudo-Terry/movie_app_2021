fe63fda5636904180654c9bbaaa62f1880e7e728
sudo-Terry <76080411+sudo-Terry@users.noreply.github.com>
2021-03-16 14:51:08 +0900

    movie app ch.7
    do it react clone coding for making movie apps chapter 7

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({title, year, summary, poster}){
+function Movie({title, year, summary, poster, genres}){
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie_data">
-        <h3 class="movie_title">{title}</h3>
-        <h5 class="movie_year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres"> 
+          {genres.map((genre, index) => {
+            return <li key={index} className="movie__genre">{genre}</li>
+          })}
+        </ul>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
@@ -20,6 +25,7 @@ Movie.propTypes={
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genre: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
